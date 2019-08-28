8321eeb0629c5897427e794dd1dff52907d60839
Ingveldur Dís Heiðarsdóttir <idh@sbotti.is>
2019-08-28 14:24:52 +0700

    Displaying products from contentful + styling

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { ContentfulService } from "src/app/services/contentful.service";
+import { Entry } from "contentful";
+import * as _ from "lodash";
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  selector: "app-category",
+  templateUrl: "./category.component.html",
+  styleUrls: ["./category.component.scss"]
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  public loading = true;
+  public categoryData: any;
+  public productsInCateogory: any = [];
+  constructor(private contentfulService: ContentfulService) {}
 
   ngOnInit() {
-  }
+    this.contentfulService
+      .getCheeseType("blue-cheese")
+      .then(res => {
+        this.categoryData = res.fields;
+        console.log("categoryData", this.categoryData);
 
+        this.categoryData.categories.forEach(element => {
+          if (element.fields.products) {
+            this.productsInCateogory = this.productsInCateogory.concat(
+              element.fields.products
+            );
+          }
+        });
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
 }
