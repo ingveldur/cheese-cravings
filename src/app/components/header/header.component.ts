7d668cf3aa3d6ca12ca04181534418838a5d6ea1
Ingveldur Dís Heiðarsdóttir <idh@sbotti.is>
2019-09-12 18:17:34 +0700

    toggle background of header based on url

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute, Params, Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent implements OnInit {
-  @Input() withBackgroundColor = true;
-  constructor() {}
-
-  ngOnInit() {}
+export class HeaderComponent {
+  public url;
+  constructor(private router: Router) {
+    this.router.events.subscribe(ev => {
+      if (ev instanceof NavigationEnd) {
+        this.url = ev.url;
+      }
+    });
+  }
 }
